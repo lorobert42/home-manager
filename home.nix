b47c216277b85fbca9de69272d568e5ddb62e4a1
lorobert <lorobert@student.42lausanne.ch>
2024-11-12 19:47:40 +0100

    Node + Rust

--- home.nix
+++ home.nix
@@ -21,6 +21,12 @@
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
+    pkgs.rustc
+    pkgs.cargo
+    pkgs.rustfmt
+    pkgs.rust-analyzer
+    pkgs.clippy
+    pkgs.nodejs_22
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
